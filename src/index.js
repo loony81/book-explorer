71ad9884fb16a0b24ba77681a6c432e27fac8d08
Roman <r.nazarenko.81@gmail.com>
2019-02-23 17:11:50 +0200

    import global component inside index.js and render it onto index.html

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from './js/components/Global';
+
+
+
+ReactDOM.render(
+  <Global />,
+  document.getElementById('root')
+);
