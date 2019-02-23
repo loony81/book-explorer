2af61bba2ee1ab7cfe99e591871a7901051ea506
Roman <r.nazarenko.81@gmail.com>
2019-02-23 18:34:15 +0200

    import all necessary components from react-bootstrap and createa form, search input field and submit button

--- src/js/components/Global.js
+++ src/js/components/Global.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import {Form, Button, InputGroup, FormControl} from 'react-bootstrap';
 
 class Global extends Component {
 	render(){
 		return (
-			<div>The Global App component!</div>
+			<div>
+				<h2>Book Explorer!</h2>
+				
+	  				<Form.Group controlId="formBasicInput">
+					<InputGroup>
+					    <FormControl
+					      placeholder="Search for a book"
+					      aria-label="Search for a book"
+					    />
+					    <InputGroup.Append >
+					      <Button variant="outline-primary">Search</Button>
+					    </InputGroup.Append>
+					  </InputGroup>
+					  </Form.Group>
+				  
+			</div>
 		)
 	}
 }
