82c8e00416f0029bf84f622071af0259c24d28b2
Roman <r.nazarenko.81@gmail.com>
2019-03-01 17:46:29 +0200

    add mini-css-extract-plugin to be able to compile style modules into separate css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       },
+//       {
+//         test:/\.css$/,
+//         use:['style-loader','css-loader']
+//       }
+//     ]
+//   }
+// };
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -10,8 +30,13 @@ module.exports = {
       },
       {
         test:/\.css$/,
-        use:['style-loader','css-loader']
+        use:[MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]
 };
