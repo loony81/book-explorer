a50a855ef6c7a97866e6b07f6db4bd0523a16c4b
Roman <r.nazarenko.81@gmail.com>
2019-03-01 19:19:23 +0200

    add additional plugins to minimize css bundle, add optimization option inside webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,21 @@
 //     ]
 //   }
 // };
-
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
   module: {
     rules: [
       {
